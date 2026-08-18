e573331104177c7eea121b0c071e1c8203c7b3ed
official-taufiq <official.taufiqrahman@gmail.com>
2026-08-18 17:54:00 +0530

    second iter

--- split_test.go
+++ split_test.go
@@ -13,3 +13,24 @@ func TestSplit(t *testing.T) {
 		t.Fatalf("Expected: %v, got:%v", want, got)
 	}
 }
+
+func TestSplit2(t *testing.T) {
+	type test struct {
+		input string
+		sep   string
+		want  []string
+	}
+
+	tests := []test{
+		{input: "a/b/c", sep: "/", want: []string{"a", "b", "c"}},
+		{input: "a/b/c", sep: ",", want: []string{"a/b/c"}},
+		{input: "abc", sep: "/", want: []string{"abc"}},
+	}
+
+	for _, v := range tests {
+		got := Split(v.input, v.sep)
+		if !reflect.DeepEqual(v.want, got) {
+			t.Fatalf("Expected: %v, Got: %v", v.want, got)
+		}
+	}
+}
